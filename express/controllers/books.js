400ab1c4091547304d0cd501944078c69ef2603b
nairyasatryan <nairyasatryan@gmail.com>
2022-09-29 09:44:18 +0400

    last changes

--- express/controllers/books.js
+++ express/controllers/books.js
@@ -1,55 +1,83 @@
-import { v4 as uuidv4 } from "uuid";
 
+import sqlite from "sqlite3";
+const SQlite3 = sqlite.verbose();
 let books = [];
+const db = new SQlite3.Database("bookData.db");
+
+db.run(
+  "CREATE TABLE IF NOT EXISTS books (author TEXT NOT NULL, title TEXT NOT NULL, language TEXT NOT NULL, status TEXT NOT NULL, id INTEGER PRIMARY KEY AUTOINCREMENT )"
+);
 export const getBooks = (req, res) => {
-    res.send(books);
-  }
- 
- export const addBook = (req, res) => {
-    const book = req.body;
-  
-    books.push({ ...book, id: uuidv4() });
-  
-    res.send(
-      `The book ${book.title}  with all inforamtion added to the database!`
-    );
-  }
+  res.send(books);
+};
 
-  export const getBook = (req, res) => {
-    const { id } = req.params;
-  
-    const foundBook = books.find((book) => book.id === id);
-    res.send(foundBook);
-  }
+export const addBook = (req, res) => {
+  let author = req.body["author"];
+  let title = req.body["title"];
+  let language = req.body["language"];
+  let status = req.body["status"];
 
-  export const deleteBook = (req, res) => {
-    const { id } = req.params;
-  
-    books = books.filter((book) => book.id !== id);
-    res.send(`Book with the id ${id} deleted from the database`);
-  }
+  // const book = {
+  //   author: author,
+  //   title: title,
+  //   language: language,
+  //   status: status,
+  // };
 
-  export const updateBook = (req, res) => {
-    const { id } = req.params;
-    const { author, title, language, status } = req.body;
-  
-    const book = books.find((book) => book.id === id);
-  
-    if (author) {
-      book.author = author;
-    }
-  
-    if (title) {
-      book.title = title;
-    }
-  
-    if (language) {
-      book.language = language;
-    }
-  
-    if (status) {
-      book.status = status;
-    }
-  
-    res.send(`Book with the id ${id} has been updated`);
-  }
+  // books.push({ ...book, id: nanoid() });
+  db.run(
+    "INSERT INTO books (author, title, language, status, id) VALUES(?,?,?,?,?)",
+    author,
+    title,
+    language,
+    status,
+    
+  );
+
+  res.send(
+    `The book ${title}  with all inforamtion added to the database!`
+  );
+};
+
+export const getBook = (req, res) => {
+  const  id  = req.params;
+
+  const foundBook = books.find((book) => book.id === id);
+  res.send(foundBook);
+};
+
+export const updateBook = (req, res) => {
+  const  {id}  = req.params;
+  let author = req.body["author"];
+  let title = req.body["title"];
+  let language = req.body["language"];
+  let status = req.body["status"];
+
+ let sql = `UPDATE books SET author = ?, title = ? , language = ?, status =? WHERE id = ${id}`
+ db.run(sql, author, title, language, status,)
+
+  // if (author) {
+  //   book.author = author;
+  // }
+
+  // if (title) {
+  //   book.title = title;
+  // }
+
+  // if (language) {
+  //   book.language = language;
+  // }
+
+  // if (status) {
+  //   book.status = status;
+  // }
+
+  res.send(`Book with the id ${id} has been updated`);
+};
+
+export const deleteBook = (req, res) => {
+  const {id}  = req.params;
+  let sql = "DELETE FROM books WHERE id = ? ";
+  db.run(sql, id)
+  res.send(`Book with the id ${id} deleted from the database`);
+};
--- express/index.js
+++ express/index.js
@@ -2,18 +2,17 @@ import express from "express";
 import bodyParser from "body-parser";
 import usersRoutes from "./routes/books.js";
 
-
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
- 
-app.use('/books', usersRoutes);
 
-app.get('/', (req, res) =>{
+app.use("/books", usersRoutes);
 
-    res.send('Hello from Homepage')
+app.get("/", (req, res) => {
+  res.send("Hello from Homepage");
 });
 
-app.listen(PORT, () => console.log(`Server is running on port: http://localhost:${PORT}`));
-
+app.listen(PORT, () =>
+  console.log(`Server is running on port: http://localhost:${PORT}`)
+);
